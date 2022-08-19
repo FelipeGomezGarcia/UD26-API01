9fbdda1db5a43c6d96333ddb3327d84b8b5c7124
FelipeGomezGarcia <feli.goga@gmail.com>
2022-08-19 11:05:19 +0200

    Proyecto vacio

---
+++ src/main/java/com/example/api_rest/Ud2601Application.java
@@ -0,0 +1,13 @@
+package com.example.api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ud2601Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ud2601Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api_rest/Ud2601ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ud2601ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
